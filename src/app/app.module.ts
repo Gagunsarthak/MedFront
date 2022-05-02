7892cc9ca6c3fdec4376d3ee21a3d6b6b6ddfec9
Gagunsarthak <saisarthakmohanty1996@gmail.com>
2022-05-02 16:07:34 +0530

    updating to fusion code structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,54 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSliderModule } from '@angular/material/slider';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { AppProfileComponent } from 'src/routes/app-profile/app-profile.component';
+import { RootComponent } from './component/root/root.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatRippleModule} from '@angular/material/core';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatTooltipModule} from '@angular/material/tooltip';
+// import { AppNavBarComponent } from './component/app-nav-bar/app-nav-bar.component';
+import { AppPublicNavBarComponent } from './component/app-public-nav-bar/app-public-nav-bar.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    // AppComponent
+    RootComponent,
+    // AppNavBarComponent,
+    AppPublicNavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatSliderModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatProgressBarModule,
+    MatExpansionModule,
+    MatRippleModule,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatTooltipModule,
     BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RootComponent]
 })
 export class AppModule { }
