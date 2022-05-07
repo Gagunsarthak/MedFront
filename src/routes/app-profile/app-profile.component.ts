cff040cb4793a8b3d4028e4cf2e784b30976c65f
Gagunsarthak <saisarthakmohanty1996@gmail.com>
2022-05-08 02:09:06 +0530

    profile about section

--- src/routes/app-profile/app-profile.component.ts
+++ src/routes/app-profile/app-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-app-profile',
@@ -6,12 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-profile.component.scss']
 })
 export class AppProfileComponent implements OnInit {
-
+  public getScreenWidth: any;
+  public getScreenHeight: any;
+  isMobileview: boolean=false;
   constructor() { 
     console.log("Comp reached")
   }
 
   ngOnInit(): void {
+    this.getScreenWidth = window.innerWidth;
+    this.getScreenHeight = window.innerHeight;
+    this.onWindowResize()
   }
-
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+    this.getScreenHeight = window.innerHeight;
+    //console.log("Width of screen is ",this.getScreenWidth)
+    if(this.getScreenWidth<775){
+      this.isMobileview=true
+    }else{
+      this.isMobileview=false
+    }
+  // this.openDialog()
+  }
 }
