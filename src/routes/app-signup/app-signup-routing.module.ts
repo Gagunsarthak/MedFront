3ca8b86a7dd7b5d4d0738c2ab24bf71b172e1a54
Gagunsarthak <saisarthakmohanty1996@gmail.com>
2022-06-05 16:08:31 +0530

    signup forms, work in progress

--- src/routes/app-signup/app-signup-routing.module.ts
+++ src/routes/app-signup/app-signup-routing.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppSignupComponent } from './app-signup.component';
+import { AppLoginComponent } from './components/app-login/app-login.component';
+import { AppRegisterComponent } from './components/app-register/app-register.component';
 
 
 const routes: Routes = [
-  {path: '',  component: AppSignupComponent},
+  {path: '',pathMatch: 'full' , redirectTo: '/signUp/login'},
+  {path: 'login',  component: AppLoginComponent},
+  {path: 'register',  component: AppRegisterComponent},
   
   
 ];
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild([   {
+    path: '',
+    component: AppSignupComponent,
+    // resolve: {
+    //   content: ModeratorTimelineService,
+    // },
+    children: routes,
+  },])],
   exports: [RouterModule]
 })
 export class AppSignupRoutingModule { }
--- src/routes/app-signup/app-signup.module.ts
+++ src/routes/app-signup/app-signup.module.ts
@@ -3,14 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { AppSignupRoutingModule } from './app-signup-routing.module';
 import { AppSignupComponent } from './app-signup.component';
+import { AppLoginComponent } from './components/app-login/app-login.component';
+import { AppRegisterComponent } from './components/app-register/app-register.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppSignupComponent
+    AppSignupComponent,
+    AppLoginComponent,
+    
+    AppRegisterComponent
   ],
   imports: [
     CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppSignupRoutingModule
   ]
 })
